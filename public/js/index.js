e529cab6fff5aab99297714bc24b5dc535608efa
Sanjay M G <sanjay.m.g@ibm.com>
2021-11-10 15:32:15 +0530

    Added custom alert for booked tour

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ import { displayMap } from './mapBox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
@@ -67,3 +68,6 @@ if (bookBtn) {
     bookTour(tourId);
   });
 }
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);
