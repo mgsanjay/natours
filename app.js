c33095fabbd84a276277afc1a2f6ff026ad3b67b
Sanjay M G <sanjay.m.g@ibm.com>
2021-11-10 15:11:04 +0530

    Added stripe webhook

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
@@ -57,6 +58,13 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
+
+//Since data should be raw and not json we have the route here
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 //Body parser, reading data from body to req.body
 app.use(express.json({ limit: '10kb' })); //max 10kb body
