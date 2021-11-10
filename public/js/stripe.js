1a047a9e0ca752d8d5fd2a606a8484b5a637f53a
Sanjay M G <sanjay.m.g@ibm.com>
2021-11-10 11:19:21 +0530

    updated routes and cleanup

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,9 +7,7 @@ export const bookTour = async (tourId) => {
       'pk_test_51Ju5WHSGU8S676CyLJ5NJaqz2LYrYGgKcPWmjkifOqsHdxeqMKnoXT8uMIVo7Cf388HcnTjoUKooEyfrCgIsMUix00zKkNR1Nn'
     );
     //1.Get checkout session from the API
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     //2.Create checkout form + charge credit card
     await stripe.redirectToCheckout({ sessionId: session.data.session.id });
   } catch (err) {
